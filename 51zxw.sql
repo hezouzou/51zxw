c562858f6c3d53df8901a7a47fef4f26e630f459
HeZou <304515735@qq.com>
2019-01-21 20:46:07 +0800

    删除之前课程分类和详情页面 添加新页面

--- 51zxw.sql
+++ 51zxw.sql
@@ -23,29 +23,29 @@ INSERT INTO zx_clan VALUES
 (null,'会计课程');
 
 -- 创建分类自学课程
-CREATE TABLE zx_laptop(
+CREATE TABLE zx_study(
 	lid INT  PRIMARY KEY AUTO_INCREMENT,
 	lname VARCHAR(64),
 	classify VARCHAR(64),
 	lecturer VARCHAR(64),
 	crowd  VARCHAR(128),
 	softver VARCHAR(64),
-	basic VARCHAR(64),
-	total INT, --总节数
-	peocou INT, --学习总人数
+	basii VARCHAR(64),
+	total INT, 
+	peocou INT, 
 	cid INT
 );
 
 -- 插入分类自学课程
-INSERT INTO zx_laptop VALUES
+INSERT INTO zx_study VALUES
 (null,"计算机基础知识教程","初级","SAM 老师","电脑初学者","零基础","WIN XP，WIN7",140,0,1),
 (null,"五笔打字视频教程","初级","李雨阳 老师","不会打字的学员，或会其它输入法，想转五笔输入法","零基础","王码五笔86版，金山打字通2003",43,0,1),
 (null,"word2016基础视频教程","初级","黄建彬 老师","想学习信息化办公的学员，学习word图文排版的学员","有电脑打字基础","Word2016中文版",232,0,1),
 (null,"Excel2016基础视频教程","初级","黄建彬 老师","想学习信息化办公的学员，学习Excel电子表格的学员","有电脑打字基础","Excel 2016中文版",226,0,1),
 (null,"Excel2013函数大全教程","高级","曾贤志 老师","想提高Excel函数水平的学员","Excel基础","office2013中文版",232,0,1),
 (null,"Excel VBA 2013视频教程","高级","曾贤志 老师","想学习Excel VBA编程的学员","Excel基础","office2013中文版",158,0,1),
-(null,"PPT2016基础视频教程","初级","黄建彬 老师","想学习信息化办公的学员，学习PPT的学员","零基础","PPT2016中文版",1),
-(null,"WPS2013文字教程","进阶","曾贤志 老师","想学习信息化办公的学员，学习WPS的学员","零基础","	WPS2013中文版",201,0,1),
+(null,"PPT2016基础视频教程","初级","黄建彬 老师","想学习信息化办公的学员，学习PPT的学员","零基础","PPT2016中文版",201,0,1),
+(null,"WPS2013文字教程","进阶","曾贤志 老师","想学习信息化办公的学员，学习WPS的学员","零基础","	WPS2013中文版",111,0,1),
 (null,"Acrobat2015视频教程","进阶","付学军 老师","经常需要使用和编辑PDF文件的学员","有office软件基础","Acrobat DC 2015中文版",118,0,1),
 (null,"iebook电子杂志制作教程","初级","舒清 老师","需要做多媒体电子书的学员","零基础","iebook 6.0",32,0,1),
 (null,"WIN7视频教程","初级","刘国锋 老师","电脑爱好者","零基础","Windows7",19,0,1),
@@ -70,10 +70,51 @@ INSERT INTO zx_laptop VALUES
 (null,"欧式家具建模视频教程","高级","李澍 老师","	有志从事欧式家具建模和表现的学员","需有3ds max多边形建模基础","3Ds max2013中文版",156,0,3);
 
 -- 创建自学课程图表
-CREATE TABLE zx_laptop_pic(
+CREATE TABLE zx_study_pic(
 	pid INT PRIMARY KEY AUTO_INCREMENT,
 	lid INT,
 	title VARCHAR(128),
 	sm VARCHAR(128),
 	md VARCHAR(128)
-)
+);
+
+-- 创建课程信息表
+CREATE TABLE zx_study_details(
+    did INT PRIMARY KEY AUTO_INCREMENT,
+	lid INT,
+	title VARCHAR(64),
+	herf VARCHAR(128),
+	upTime DATE
+);
+
+INSERT INTO zx_study_details VALUES
+(null,3,"1-1、Word2016简介",null,"2018-12-25"),
+(null,3,"1-2、Word2016新特性",null,"2018-12-25"),
+(null,3,"1-3、尝试创建一份通知",null,"2018-12-25"),
+(null,3,"1-4、Word2016知识体系和制作流程",null,"2018-12-25"),
+(null,3,"1-5、快速认识Word2016的操作环境",null,"2018-12-25"),
+(null,3,"1-6、定制个性化的办公环境（一）",null,"2018-12-25"),
+(null,3,"1-7、定制个性化的办公环境（二）",null,"2018-12-25"),
+(null,3,"1-8、通用功能及一种功能多种操作法",null,"2018-12-25"),
+(null,3,"1-9、Word文档的相关操作（一）",null,"2018-12-25"),
+(null,3,"1-10、Word文档的相关操作（二）",null,"2018-12-25"),
+(null,3,"1-11、Word2016的五种视图",null,"2018-12-25"),
+(null,3,"1-12、Word2016的显示比例",null,"2018-12-25"),
+(null,3,"1-13、在Word2016中输入文本",null,"2018-12-25"),
+(null,3,"1-14、文本的插入、改写与删除",null,"2018-12-25"),
+(null,3,"1-15、撤消、恢复和重复",null,"2018-12-25"),
+(null,3,"1-16、选定文本的多种操作方法（一）",null,"2018-12-25"),
+(null,3,"1-17、选定文本的多种操作方法（二）",null,"2018-12-25"),
+(null,3,"1-18、文本的移动与复制",null,"2018-12-25"),
+(null,3,"1-19、设置字体和字号",null,"2018-12-25"),
+(null,3,"2-1、插入或绘制表格",null,"2018-12-25"),
+(null,3,"2-2、了解表格的相关概念",null,"2018-12-25"),
+(null,3,"2-3、输入文本和选定内容",null,"2018-12-25"),
+(null,3,"2-4、行列的编辑操作（一）",null,"2018-12-25"),
+(null,3,"2-5、行列的编辑操作（二）",null,"2018-12-25"),
+(null,3,"2-6、设置行高和列宽（一）",null,"2018-12-25"),
+(null,3,"2-7、设置行高和列宽（二）",null,"2018-12-25"),
+(null,3,"2-8、单元格的合并与拆分",null,"2018-12-25"),
+(null,3,"2-9、表格的快速样式",null,"2018-12-25"),
+(null,3,"3-1、图形对象的类别和来源",null,"2018-12-25"),
+(null,3,"3-2、艺术字的文字效果",null,"2018-12-25");
